320506ac8a0d646a7de492c9213ee9d610916f0b
Samuel Thomas <sgpthomas@gmail.com>
2021-11-24 12:00:27 -0600

    some changes

--- scripts/filter.py
+++ scripts/filter.py
@@ -5,6 +5,8 @@ import json
 import sexpdata as sexp
 from dataclasses import dataclass
 from typing import List, Any
+import networkx as nx
+import itertools
 
 
 @dataclass
@@ -58,6 +60,20 @@ def parse(raw):
             raise Exception(f"Unknown class: {head}, ({raw})")
     else:
         return Lit(raw)
+
+
+def pprint(prog):
+    if type(prog) == VecOp or type(prog) == Op:
+        return "({} {})".format(prog.op, " ".join([pprint(c) for c in prog.children]))
+    elif type(prog) == Lit:
+        if type(prog.item) == sexp.Symbol:
+            return "{}".format(prog.item._val)
+        else:
+            return "{}".format(prog.item)
+    elif type(prog) == Vec:
+        return "(Vec {})".format(" ".join([pprint(c) for c in prog.children]))
+    else:
+        raise Exception(f"Unknown type: {prog}")
 
     
 COSTS = {
@@ -119,30 +135,70 @@ def tree_depth(prog):
         raise Exception(f"unknown: {prog}")
 
 
+def operators(prog):
+    """Get the operators of the prog tree."""
+    if type(prog) == VecOp or type(prog) == Op:
+        return sum([operators(c) for c in prog.children], [prog.op])
+    elif type(prog) == Vec:
+        return sum([operators(c) for c in prog.children], [])
+    elif type(prog) == Lit:
+        return []
+    else:
+        raise Exception(f"Unknown: {prog}")
+
+
+def dependency_graph(ruleset):
+    """Build a graph where the nodes in the graph are all the operators covered
+    by the rewrite rules and there is an edge between two nodes if there is a rewrite
+    rule that constructs the second node from the first."""
+
+    G = nx.Graph()
+
+    for (lhs, rhs) in ruleset:
+        lops = operators(lhs)
+        rops = operators(rhs)
+
+        G.add_nodes_from(lops)
+        G.add_nodes_from(rops)
+
+        G.add_edges_from(itertools.product(lops, rops))
+
+    return G
+        
+
 def main():
     """Become the main function."""
     filename = sys.argv[1]
     with open(filename, "r") as f:
         j = json.load(f)
-        # limit = 0
         n_kept = 0
+        ruleset = []
         for eq in j["eqs"]:
             # print(f"{eq['lhs']} <=> {eq['rhs']}")
             lhs = parse(sexp.loads(eq["lhs"]))
             rhs = parse(sexp.loads(eq["rhs"]))
+            ruleset.append((lhs, rhs))
+
+        limit = 0
+        for (lhs, rhs) in ruleset:
             # if getheads(lhs) != getheads(rhs):
-            cost_diff = abs(cost(lhs) - cost(rhs)) > 2
+            c = abs(cost(lhs) - cost(rhs))
+            cost_diff = c > 2
             all_vars = len(set(leaves(lhs))) == 4 and len(set(leaves(rhs))) == 4
-            vec_mac = (uses_vec_mac(lhs) and tree_depth(lhs) == 1) or (tree_depth(rhs) == 1 and uses_vec_mac(rhs))
-            if vec_mac:
+            vec_mac_lhs = (uses_vec_mac(lhs) and tree_depth(lhs) == 1)
+            vec_mac_rhs = (tree_depth(rhs) == 1 and uses_vec_mac(rhs))
+            vec_mac = vec_mac_lhs or vec_mac_rhs
+            if cost_diff:
                 n_kept += 1
-                print(f"{eq['lhs']} <=> {eq['rhs']}")
-                print("cost: {}".format(abs(cost(lhs) - cost(rhs))))
+                print(f"[{c}] {pprint(lhs)} <=> {pprint(rhs)}")
 
             # if limit > 3:
             #     break
             # limit += 1
         print(f"# kept/total: {n_kept} / {len(j['eqs'])}")
+
+        # G = dependency_graph(ruleset)
+        # nx.nx_agraph.write_dot(G, "a.dot")
 
 
 if __name__ == "__main__":
