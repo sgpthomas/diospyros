12d6891134c2ec1c3cacd9ec26fb5b9f6883c46d
Samuel Thomas <sgpthomas@gmail.com>
2022-04-27 10:40:35 -0500

    add some junk to the sync script

--- sync.sh
+++ sync.sh
@@ -13,5 +13,10 @@ rsync -rP --exclude=.git \
       --exclude=experiments/res* \
       --exclude=experiments/*.csv \
       --exclude=experiments/*.org \
+      --exclude=time-ablation* \
+      --exclude=variable_dup_abl_n_ops \
+      --exclude=vd_abl_n_op2_vec_size \
+      --exclude=vec \
+      --exclude=t*.json \
       -e "ssh -i ~/.ssh/thelio.pem" \
       `pwd`/ "ubuntu@$ADDR:~/$dir/"
